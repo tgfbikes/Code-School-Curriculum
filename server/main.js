7f79a26a2a86bf7e365c35ccb6db9b9a6533febd
Stephen King <stephen@rocketmade.com>
2016-04-23 14:53:00 -0600

    set up static files

--- server/main.js
+++ server/main.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app  = new express();
 var port = 7777;
 
+app.use(express.static(__dirname + '/../app'));
+
 app.get('/', function(req, res) {
   res.render('../app/index.ejs', {});
 })
