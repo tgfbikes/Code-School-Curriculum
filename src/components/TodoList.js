162100b830d46884a59bcd6ba65cc169de004c4f
Stephen King <stephen@rocketmade.com>
2016-04-24 10:02:34 -0600

    created components directory and TodoList component

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoList = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>TodoList working</p>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoList;
