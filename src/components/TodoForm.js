24e0cc6de647b516062c85bca38f2106c66da50b
Stephen King <stephen@rocketmade.com>
2016-04-24 15:05:01 -0600

    initial set up for the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,12 +2,22 @@
 
 var React = require('react');
 
-
 var TodoForm = React.createClass({
   render: function() {
     return (
       <div className="col-md-6">
         <h2>Add a Task</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input type="text" className="form-control" id="title" placeholder="Todo title" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Password</label>
+            <input type="text" className="form-control" id="description" placeholder="description" />
+          </div>
+          <button type="submit" className="btn btn-default">Submit</button>
+        </form>
       </div>
     );
   }
