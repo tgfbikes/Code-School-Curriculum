d529d2503ccd40736264ecd5dd582f85a499529a
Stephen King <stephen@rocketmade.com>
2016-04-24 14:44:17 -0600

    created TodoForm component, initial set up

---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+
+var TodoForm = React.createClass({
+  render: function() {
+    return (
+      <div className="col-md-6">
+        <h2>Add a Task</h2>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoForm;
