68d1d6ac3470584bf99a054732d972ba990792f0
Stephen King <stephen@rocketmade.com>
2016-04-24 14:45:24 -0600

    added some bootstrap for alignment

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
+
 $ = jQuery = require('jquery');
 var React = require('react');
 var TodoList = require('./components/TodoList');
+var TodoForm = require('./components/TodoForm');
 
- var App = React.createClass({
-   render: function() {
-     return (
-       <div>
+var App = React.createClass({
+ render: function() {
+   return (
+     <div className="container">
+       <div className="row">
+         <TodoForm />
          <TodoList />
        </div>
-     );
-   }
- });
+     </div>
+   );
+ }
+});
 
- React.render(<App />, document.getElementById('app'));
+React.render(<App />, document.getElementById('app'));
