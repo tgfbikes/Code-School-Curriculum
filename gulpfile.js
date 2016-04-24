edb72a7cc030bbd65c4d9b59b48e7501fa84a8f8
Stephen King <stephen@rocketmade.com>
2016-04-24 09:32:28 -0600

    revamped gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var gulp = require('gulp');
-var connect = require('gulp-connect'); //Runs a local dev server
-var open = require('gulp-open'); //Open a URL in a web browser
+var gulp 			 = require('gulp');
+var connect 	 = require('gulp-connect'); //Runs a local dev server
+var open 			 = require('gulp-open'); //Open a URL in a web browser
 var browserify = require('browserify'); // Bundles JS
-var reactify = require('reactify');  // Transforms React JSX to JS
-var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
-var concat = require('gulp-concat'); //Concatenates files
-var lint = require('gulp-eslint'); //Lint JS files, including JSX
+var reactify   = require('reactify');  // Transforms React JSX to JS
+var source     = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+var concat     = require('gulp-concat'); //Concatenates files
+var lint 			 = require('gulp-eslint'); //Lint JS files, including JSX
 
 var config = {
 	port: 9005,
