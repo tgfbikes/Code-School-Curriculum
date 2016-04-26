5b3b0fa72ee386aae43c1b209b5ad50a1a661616
Stephen King <stephen@rocketmade.com>
2016-04-26 09:02:54 -0600

    added path for main.css in assets

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,8 @@ var config = {
 		js: './src/**/*.js',
 		css: [
       		'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+      		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+					'./src/assets/**/*.css'
     	],
 		dist: './dist',
 		mainJs: './src/main.js'
