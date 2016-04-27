fae3764599affeaff581bfa5bb418575ee2d5b09
Stephen King <stephen@rocketmade.com>
2016-04-26 18:03:58 -0600

    onclick binding to nothing to keep this of Todo controller view and passing index

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,14 +4,15 @@ var React = require('react');
 
 var TodoListItem = React.createClass({
   render: function() {
+    var index = this.props.index;
     return (
       <li className="todo__list-item">
         <span><strong>{this.props.todoData.title}</strong>
         <br />
         - {this.props.todoData.description}</span>
         <div className="pull-right">
-          <a className="btn btn-warning btn-sm">Edit</a>
-          <a className="btn btn-danger btn-sm">Delete</a>
+          <div className="btn btn-warning btn-sm" onClick={this.props.editTodo.bind(null, index)}>Edit</div>
+          <div className="btn btn-danger btn-sm" onClick={this.props.deleteTodo.bind(null, index)}>Delete</div>
         </div>
       </li>
     );
