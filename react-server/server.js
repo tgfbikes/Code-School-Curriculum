424d9735455ef32bedc8257bed5adce7c5f00a5a
Stephen King <stephen@rocketmade.com>
2016-04-29 09:26:20 -0600

    messed around with path to static files, finally got it

--- react-server/server.js
+++ react-server/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
+var path = require('path');
+var jade = require('jade');
 var app = new express();
 
-// Set up static routes
-app.use('/static', express.static(__dirname + '/views'));
+// Use jade for template engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'jade');
 
-// Use HTML for template engine
-app.set('views', './views');
-app.set('view engine', 'exphbs');
+// Set up static routes
+app.use(express.static(__dirname + '/views'));
 
 // Routes for application
 var appRouter = require('./routes/index');
