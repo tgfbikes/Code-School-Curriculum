bcacfa9c839e1cd7ecad0e3f86446af4c8acfa94
Stephen King <stephen@rocketmade.com>
2016-04-29 08:47:16 -0600

    refactored to use handlebars

--- react-server/server.js
+++ react-server/server.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = new express();
+
+// Set up static routes
+app.use('/static', express.static(__dirname + '/views'));
+
+// Use HTML for template engine
+app.set('views', './views');
+app.set('view engine', 'exphbs');
 
 // Routes for application
 var appRouter = require('./routes/index');
@@ -7,6 +15,6 @@ var appRouter = require('./routes/index');
 app.use('/', appRouter);
 
 app.listen(3000, function() {
-  console.log('Server running..')
-  console.log('Listening on port 3000...')
+  console.log('Server running..');
+  console.log('Listening on port 3000...');
 });
