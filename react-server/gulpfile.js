8765158e6c990eb8345ea9a1baa1a3f9f814ec30
Stephen King <stephen@rocketmade.com>
2016-04-29 10:32:23 -0600

    added dependentcies and created scss and js tasks

--- react-server/gulpfile.js
+++ react-server/gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil        = require('gulp-util');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat       = require('gulp-concat');
 
 var config = {
   paths: {
@@ -11,4 +15,23 @@ var config = {
   }
 };
 
-gulp.task('default', ['jade']);
+gulp.task('scss', function() {
+  return gulp.src(config.paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function() {
+  browserify(config.paths.js)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.public + '/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['scss', 'js']);
