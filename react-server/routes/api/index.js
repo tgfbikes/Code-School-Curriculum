887c6b3949e8e771527c81c99d6f4632923efcd1
Stephen King <stephen@rocketmade.com>
2016-04-29 20:35:23 -0600

    added show, create, and destroy routes to associated TodoCtrl methods

--- react-server/routes/api/index.js
+++ react-server/routes/api/index.js
@@ -4,5 +4,10 @@
 var TodoCtrl = require('../../controllers/TodoCtrl');
 
 module.exports = function (app) {
+
   app.get('/todos.json', TodoCtrl.index);
+  app.get('/todos/:id.json', TodoCtrl.show);
+  app.post('/todos/:id.json', TodoCtrl.create);
+  app.delete('/todos/:id.json', TodoCtrl.destory);
+
 };
