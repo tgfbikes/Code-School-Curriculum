893f68d8675533f85e6db6937e6c386d90ebb435
Stephen King <stephen@rocketmade.com>
2016-04-29 20:16:59 -0600

    added comments for possible students

--- react-server/controllers/MainCtrl.js
+++ react-server/controllers/MainCtrl.js
@@ -3,9 +3,9 @@
 var MainCtrl = {
 
   index: function (req, res) {
-    res.render('index');
-  }
-
-};
+    res.render('index'); // Remember, we are able to just say 'index' because Express already
+  }                      // knows we are using jade since we set that up in server.js.
+                         // Furthermore, we don't have to put the path to index.jade because
+};                       // we set up our static files.
 
 module.exports = MainCtrl;
--- react-server/controllers/TodoCtrl.js
+++ react-server/controllers/TodoCtrl.js
@@ -4,19 +4,17 @@ var Todo = require('../models/Todo');
 
 var TodoCtrl = {
 
+  // Get all todos
   index: function (req, res) {
-    res.json({
-      message: 'this is working'
+    Todo.find({}, function (err, todos) {
+      if (err) {
+        res.statusCode(500).json({ // 500 tells user something went wrong on our end
+          message: 'Data not found' // respond with json so front end can error handle
+        });
+      } else {
+        res.json(todos);
+      }
     });
-    // Todo.find({}, function (err, todos) {
-    //   if (err) {
-    //     res.statusCode(500).json({
-    //       message: 'Data not found'
-    //     });
-    //   } else {
-    //     res.json(todos);
-    //   }
-    // });
   }
 
 };
