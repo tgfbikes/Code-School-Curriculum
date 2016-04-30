de2756f43bcc968ba52897a13190d90365275c94
Stephen King <stephen@rocketmade.com>
2016-04-30 09:43:53 -0600

    all routes tested and working

--- react-server/routes/api/index.js
+++ react-server/routes/api/index.js
@@ -8,6 +8,6 @@ module.exports = function (app) {
   app.get('/api/todos.json', TodoCtrl.index);
   app.get('/api/todos/:id.json', TodoCtrl.show);
   app.post('/api/todos.json', TodoCtrl.create);
-  app.delete('/api/todos/:id.json', TodoCtrl.destroy); // Destroy has to go in above create due to both being POST
+  app.delete('/api/todos/:id.json', TodoCtrl.destroy);
 
 };
