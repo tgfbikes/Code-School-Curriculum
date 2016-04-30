19300c6c48fae7a64f36af2a3e1c0eeeafc4bd11
Stephen King <stephen@rocketmade.com>
2016-04-29 20:17:12 -0600

    added comments

--- react-server/routes/api/index.js
+++ react-server/routes/api/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Require todo controller as handler for json api
 var TodoCtrl = require('../../controllers/TodoCtrl');
 
 module.exports = function (app) {
--- react-server/routes/main/index.js
+++ react-server/routes/main/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Require main controller as handler for / route
 var MainCtrl = require('../../controllers/MainCtrl');
 
 module.exports = function(app) {
