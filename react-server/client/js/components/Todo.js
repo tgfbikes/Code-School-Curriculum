d294c2587cb1215d236e1e7eca0e3d0442461b74
stvking <stvking@gmail.com>
2016-05-04 15:07:24 -0600

    removed ajax variables and functions form componentWillMount and moved into todoFuncs

--- react-server/client/js/components/Todo.js
+++ react-server/client/js/components/Todo.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var React    = require('react');
-var TodoList = require('./TodoList');
-var TodoForm = require('./TodoForm');
-var ajax     = require('../helpers/ajax');
+var React     = require('react');
+var TodoList  = require('./TodoList');
+var TodoForm  = require('./TodoForm');
+var ajax      = require('../helpers/ajax');
+var todoFuncs = require('../helpers/todoFuncs');
 
 var Todo = React.createClass({
 
@@ -53,35 +54,7 @@ var Todo = React.createClass({
   },
 
   componentWillMount: function () {
-    var that = this;                // Need to keep context of the Todo component in our success and error callbacks
-    var url = '/api/todos.json';    // Path to get all todos from data base
-    var data = {};
-    var success = function(data) {  // Ok, data is an object with objects inside i.e. { {...}, {...}, ... }
-      var updatedTodos = Object.assign([], that.state.todos); // We don't want to directly mess with todos on state
-      
-      for (var key in data) {                   // We iterate over the main object 'data' to get access to the other objects
-        if (data.hasOwnProperty(key)) {         // Convention for making sure we don't pull any data from prototype
-          var todoData = data[key];             // Grap the object at data[key] -- key will be the place the object is located
-          var todo = {                          // Create a new todo object literal from each object in data object
-            id: todoData._id,
-            title: todoData.title,
-            description: todoData.description,
-            done: todoData.done
-          };
-          updatedTodos.push(todo);              // Each todo we create, we want to push to updatedTodos
-        }
-      }
-      
-      that.setState({  // Set state which will cause a re-render, and we have our data from the DB, I'm excited
-        todos: updatedTodos
-      });
-      
-    };
-    var error = function (xhr, status, err) {
-      console.log(err);
-    };
-    
-    ajax(url, data, success, error, 'GET');    // Need to specify 'GET' since type default is 'POST'
+    todoFuncs.index(this);
   },
 
   handleInputChange: function(evt) {
