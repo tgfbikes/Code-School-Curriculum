22d9ed8d1e308b689fce5bb1f7cbf8f2bd186b5a
stvking <stvking@gmail.com>
2016-05-04 15:14:06 -0600

    included create functionality for create method

--- react-server/client/js/helpers/todoFuncs.js
+++ react-server/client/js/helpers/todoFuncs.js
@@ -4,8 +4,7 @@ var ajax = require('./ajax');
 
 var todoFuncs = {
   
-  index: function (self) {
-    var that = self;                // Need to keep context of the Todo component in our success and error callbacks
+  index: function (that) {
     var url = '/api/todos.json';    // Path to get all todos from data base
     var data = {};
     var success = function(data) {  // Ok, data is an object with objects inside i.e. { {...}, {...}, ... }
@@ -36,19 +35,50 @@ var todoFuncs = {
     ajax(url, data, success, error, 'GET');    // Need to specify 'GET' since type default is 'POST'
   },
   
-  show: function () {
+  show: function (that) {
     
   },
   
-  create: function () {
+  create: function (that) {
+    var newTodos = Object.assign([], that.state.todos);
+    var resetInputs = Object.assign({}, that.state.inputs);
+    var url = '/api/todos.json';
+    var data = {
+      title: that.state.inputs.title.value,
+      description: that.state.inputs.description.value,
+      done: false
+    };
+
+    resetInputs.title.value = '';
+    resetInputs.description.value = '';
+
+    var success = function (data) {
+      console.log('created todo');
+      var newTodo = {
+        id: data._id,
+        title: data.title,
+        description: data.description,
+        done: data.done
+      };
+      newTodos.push(newTodo);
+      that.setState({
+        todos: newTodos,
+        inputs: resetInputs
+      });
+    };
+    var error = function (xhr, status, err) {
+      console.log('create failed');
+      console.log(err);
+    };
+
+    ajax(url, data, success, error);
+  },
+  
+  update: function (self) {
     
   },
   
-  update: function () {
-    
-  },
-  
-  delete: function () {
+  delete: function (self) {
     
   }
   
