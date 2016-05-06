ba04b2bb5ad4d1ca2894411b320da87cafa5c2c2
stvking <stvking@gmail.com>
2016-05-06 08:55:08 -0600

    changed completed success class and setup logic for button text

--- react-server/client/js/components/TodoListItem.js
+++ react-server/client/js/components/TodoListItem.js
@@ -5,14 +5,15 @@ var React = require('react');
 var TodoListItem = React.createClass({
   render: function() {
     var id = this.props.id;
-    var todoStatus = this.props.todoData.done ? ' bg-success' : ' bg-info';
+    var todoStatus = this.props.todoData.done ? ' todo--completed' : ' bg-info';
+    var completedButtonText = this.props.todoData.done ? 'Not Completed' : 'Completed';
     return (
       <li className={"todo__list-item clearfix" + todoStatus}>
         <span><strong>{this.props.todoData.title}</strong>
         <br />
         - {this.props.todoData.description}</span>
         <div className="pull-right">
-          <div className="btn btn-success btn-sm" onClick={this.props.completedTodo.bind(null, id)}>Completed</div>
+          <div className="btn btn-success btn-sm" onClick={this.props.completedTodo.bind(null, id)}>{completedButtonText}</div>
           <div className="btn btn-danger btn-sm" onClick={this.props.deleteTodo.bind(null, id)}>Delete</div>
         </div>
       </li>
